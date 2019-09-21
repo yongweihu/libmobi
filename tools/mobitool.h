251178a27bcda72fd0b8e500445a9d1bb3e0a8fd
胡永伟 <yongweihu@163.com>
2019-09-21 13:40:07 +0800

    add all codes

---
+++ tools/mobitool.h
@@ -0,0 +1,14 @@
+//
+//  mobitool.h
+//  mobi
+//
+//  Created by Yongwei Hu on 2019/4/17.
+//  Copyright © 2019 Bartek Fabiszewski. All rights reserved.
+//
+
+#ifndef mobitool_h
+#define mobitool_h
+
+int unzip_epub_from_mobi(const char *mobipath, const char *output_dir);
+
+#endif /* mobitool_h */
